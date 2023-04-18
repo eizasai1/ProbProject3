d29d9bc4e8b8d7dae7d16637540967964bb11234
Eizaku Asai <123795659+eizasai1@users.noreply.github.com>
2023-04-18 12:51:31 -0400

    Update ProbProject3.py

--- ProbProject3.py
+++ ProbProject3.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 
 
 use_random_number_generator = True
-samples = [10, 30, 50, 100, 250, 500, 1000]
-number_of_estimates = 110
+# samples = [10, 30, 50, 100, 250, 500, 1000]
+# number_of_estimates = [110 for i in range(len(samples))]
+
+samples = [3, 9, 27, 81]
+number_of_estimates = [5, 25, 110, 550]
+
 
 #Variables used for random number generator
 x = 1000
@@ -67,40 +71,53 @@ def m_n_of_x(sample_size):
     return total / sample_size
 
 
-def get_estimates_of_m_n(files):
-    for sample_size in files:
-        file = open_file(str(sample_size) + ".txt")
-        for i in range(number_of_estimates):
-            file.write(str(round(m_n_of_x(sample_size), 4)) + "\n")
+def get_estimates_of_m_n():
+    for sample_size in range(len(samples)):
+        file = open_file(str(samples[sample_size]) + ".txt")
+        for i in range(number_of_estimates[sample_size]):
+            file.write(str(round(m_n_of_x(samples[sample_size]), 4)) + "\n")
         file.close()
 
 
-def get_sample_variance(file):
+def get_sample_variance(file, sample):
     total = 0
-    for i in range(number_of_estimates):
+    for i in range(number_of_estimates[sample]):
         line = file.readline()
-        total += abs((float(line)**2) - (mu_x**2))
-    return total / number_of_estimates
+        total += (float(line)**2)
+    return abs((total / number_of_estimates[sample]) - (mu_x**2))
 
 
 
-def get_sample_mean(file):
+def get_sample_mean(file, sample):
     total = 0
-    for i in range(number_of_estimates):
+    for i in range(number_of_estimates[sample]):
         line = file.readline()
         total += float(line)
-    return total / number_of_estimates
+    return total / number_of_estimates[sample]
+
+
+def get_z_n_values(file, sample_size):
+    file2 = open("z_n" + str(samples[sample_size]) + ".txt", 'w')
+    file2.close()
+    file2 = open("z_n" + str(samples[sample_size]) + ".txt", 'a')
+    for i in range(number_of_estimates[sample_size]):
+        m_n = float(file.readline())
+        z_n = (m_n - mu_x) / math.sqrt(var_x / math.sqrt(samples[sample_size]))
+        file2.write(str(z_n) + "\n")
 
 
 def get_sample_data():
-    for sample_size in samples:
-        file = open(str(sample_size) + ".txt", 'r')
-        mean = get_sample_mean(file)
+    for sample_size in range(len(samples)):
+        file = open(str(samples[sample_size]) + ".txt", 'r')
+        mean = get_sample_mean(file, sample_size)
         file.close()
-        file = open(str(sample_size) + ".txt", 'r')
-        variance = get_sample_variance(file)
+        file = open(str(samples[sample_size]) + ".txt", 'r')
+        variance = get_sample_variance(file, sample_size)
         file.close()
-        print(str(sample_size), "mean:", mean, "variance:", variance)
+        file = open(str(samples[sample_size]) + ".txt", 'r')
+        get_z_n_values(file, sample_size)
+        file.close()
+        print("Sample Size:", str(samples[sample_size]), "mean:", mean, "variance:", variance)
 
 
 
@@ -109,11 +126,11 @@ def make_plot():
     plt.xlabel("Sample size $n$")
     plt.ylabel("$m_{n}$")
     plt.axhline(mu_x)
-    for sample_size in samples:
-        file = open(str(sample_size) + ".txt", 'r')
-        for i in range(number_of_estimates):
+    for sample_size in range(len(samples)):
+        file = open(str(samples[sample_size]) + ".txt", 'r')
+        for i in range(number_of_estimates[sample_size]):
             line = file.readline()
-            plt.plot([sample_size], [float(line)], marker="o", markersize=2,markeredgecolor="red",markerfacecolor="red")
+            plt.plot([samples[sample_size]], [float(line)], marker="o", markersize=2,markeredgecolor="red",markerfacecolor="red")
         file.close()
     plt.show()
 
@@ -125,9 +142,9 @@ def main():
     print("mu_x:", mu_x)
     print("var_x:", var_x)
     print("sigma_x:", sigma_x)
-    get_estimates_of_m_n(samples)
-    recommeneded_sample = var_x / 10
-    print(recommeneded_sample)
+    get_estimates_of_m_n()
+    recommended_sample = var_x / 10
+    print(recommended_sample)
     get_sample_data()
     # print(get_u_values())
 
